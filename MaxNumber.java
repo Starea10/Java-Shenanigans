b84c926a3a83b7e9e296bf12fed46682faa9b2c6
Starea10 <70992029+Starea10@users.noreply.github.com>
2024-03-18 17:49:33 +0800

    Using both Ternary and Method
    This code shows how to use ternary, and applying methods as well.

---
+++ MaxNumber.java
@@ -0,0 +1,16 @@
+public class MaxNumber {
+
+    public static int max (int a, int b){
+        return (a > b) ? a : b;
+    }
+    
+        public static void main (String args[]){
+        int num1  = 25;
+        int num2 = 50;
+        int maxNumber = max(num1, num2);
+        
+            System.out.println(maxNumber);
+           
+    }
+    
+}
