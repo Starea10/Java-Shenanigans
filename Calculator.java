e7fee29e49f78755ef4e13a350323baebbd157bc
Starea10 <70992029+Starea10@users.noreply.github.com>
2024-03-18 16:19:07 +0800

    Backup of Calculator

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+    
+    /* public static int multiply(int x, int y){
+        return x * y;
+    }
+    public static void Result(int result ){
+            
+        System.out.println("The Result is: "+ result);
+    } */
+    
+/* private int result;
+
+public Calculator(){
+    result = 0;
+}
+        
+public void add(int x, int y){
+result = x + y;
+}
+
+public int getResult(){
+    return result;
+}
+
+public static void main (String args[]){
+    Calculator calc =  new Calculator();
+    calc.add(2, 7);
+    int sum = calc.getResult();
+    System.out.println("sum: " + sum);
+    
+            
+}
+}
